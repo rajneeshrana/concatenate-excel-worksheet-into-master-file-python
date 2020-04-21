a6f654d1e486da0034b243b170a81fa6265fcaa9
Rajneesh Singh <63490993+rajneeshrana@users.noreply.github.com>
2020-04-21 16:08:25 +0530

    Update read.py

--- read.py
+++ read.py
@@ -7,7 +7,7 @@ df = pd.read_excel(my_file, sheet_name=None)
 
 df1 = pd.concat(df)
 
-# path were to want master file
+# path were you want master file
 writer = pd.ExcelWriter(r'C:\Users\Pro\Desktop\excelmarj\master2.xlsx')
 
 df1.to_excel(writer)
